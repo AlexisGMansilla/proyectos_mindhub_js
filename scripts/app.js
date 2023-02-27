ca3f68acf501104c784b1caf840ace01da947a08
AlexisGMansilla <alexisgmansilla@gmail.com>
2023-02-27 01:09:32 -0300

    javascript

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+data.events.forEach((element, i ) =>{
+    
+let articles = document.querySelector(".articles");
+
+let card = document.createElement('div');
+card.classList = 'card';
+
+let eventCard = `
+        <img src=${data.events[i].image} alt="event image">
+        <div class="content">
+                <h3>${data.events[i].name}</h3>
+                <p>${data.events[i].description}</p>
+            <div class="contentPrice">
+                <p>Price: $${data.events[i].price}</p>
+                <a href="./details.html">ver más...</a>
+            </div>
+        </div>
+`;
+card.innerHTML += eventCard;
+articles.appendChild(card);
+
+});
